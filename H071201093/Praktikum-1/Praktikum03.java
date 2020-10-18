350e457cd55590c998b65dac21cb471f2766de52
Rahma <72251408+rachma1201@users.noreply.github.com>
2020-10-18 23:29:33 +0800

    Update Praktikum03.java

--- H071201093/Praktikum-1/Praktikum03.java
+++ H071201093/Praktikum-1/Praktikum03.java
@@ -1,6 +1,18 @@
-class Praktikum {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+class Praktikum03 {
+
+    public static void main(String[] args){
+        Scanner x = new Scanner(System.in);
+        // sudut elevasi ujung depan kapal dan ujung belakang kapal (90 > a >b).
+
+        double h, b, a;
+        h = 90;
+        b = Math.tan(Math.toRadians(b));
+        a = Math.tan(Math.toRadians(a));
         
+        double panjangkapal = h * (a - b) ;
+        System.out.printf("%.1f", panjangkapal, "m");
+
     }
 }
