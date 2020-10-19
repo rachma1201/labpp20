aaa5a568b46061f75c2656d6f13414b63de45c24
Rahma <72251408+rachma1201@users.noreply.github.com>
2020-10-19 08:14:16 +0800

    Update Praktikum03.java

--- H071201093/Praktikum-1/Praktikum03.java
+++ H071201093/Praktikum-1/Praktikum03.java
@@ -7,12 +7,17 @@ class Praktikum03 {
         // sudut elevasi ujung depan kapal dan ujung belakang kapal (90 > a >b).
 
         double h, b, a;
-        h = 90;
-        b = Math.tan(Math.toRadians(b));
-        a = Math.tan(Math.toRadians(a));
+        System.out.println("Masukkan tinggi kapal: ");
+        h = x.nextDouble();
+        System.out.println("Masukkan ujung depan: ");
+        b = x.nextDouble();
+        System.out.println("Masukkan ujung belakang: ");
+        a = x.nextDouble();
         
-        double panjangkapal = h * (a - b) ;
-        System.out.printf("%.1f", panjangkapal, "m");
+        double A = Math.tan(Math.toRadians(b));
+        double B = Math.tan(Math.toRadians(a));
+        double panjangkapal = h * (A - B);
+        System.out.printf("%.1f%s", panjangkapal, " m");
 
     }
 }
